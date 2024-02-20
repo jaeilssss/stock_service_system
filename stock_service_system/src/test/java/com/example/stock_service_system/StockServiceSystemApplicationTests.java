9d7ae9117a5288835614ff16c9a6a3e24958a7dc
jaeilssss <jaeilssss@gmail.com>
2024-02-20 22:23:20 +0900

    init

---
+++ stock_service_system/src/test/java/com/example/stock_service_system/StockServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_service_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
