1befe9772baaa50afd41ae19da4804ab3fa67679
jaeilssss <jaeilssss@gmail.com>
2024-02-26 17:50:50 +0900

    #2-TEST User Test 코드 구현

---
+++ stock_service_system/src/test/java/com/example/stock_service_system/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.stock_service_system.service;
+
+import com.example.stock_service_system.controller.UserController;
+import com.example.stock_service_system.dto.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+import static org.mockito.BDDMockito.given;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class UserServiceTest {
+
+}
