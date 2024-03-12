5e5b8fb21a0d66e22e726e1ff74a50cfd8eed32d
jaeilssss <jaeilssss@gmail.com>
2024-03-12 12:57:16 +0900

    ADD Gradle JWT 의존성 추가

---
+++ stock_service_system/src/main/java/com/example/stock_service_system/config/SpringSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.stock_service_system.config;
+
+import com.example.stock_service_system.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebMvcSecurity
+public class SpringSecurityConfig {
+
+    private final UserService service;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer
+                        .loginPage("/login"))
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            HttpSecurity httpSecurity,
+            BCryptPasswordEncoder bCryptPasswordEncoder,
+            UserDetailsService userDetailsService
+    ) throws Exception {
+        return httpSecurity
+                .getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(service)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
